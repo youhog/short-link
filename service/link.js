ba576b6a6598a829d50fa08ebb8487475cf61cf8
Alessandro-pang <pcy1314@163.com>
2024-12-12 09:19:57 +0800

    feat: 缩短 hash 长度

--- service/link.js
+++ service/link.js
@@ -2,7 +2,7 @@
  * @Author: zi.yang
  * @Date: 2024-12-11 19:47:42
  * @LastEditors: zi.yang
- * @LastEditTime: 2024-12-12 09:16:19
+ * @LastEditTime: 2024-12-12 09:19:42
  * @Description: 
  * @FilePath: /short-link/service/link.js
  */
@@ -13,7 +13,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
 
 function generatorHash(url) {
-  var md5 = crypto.createHash('shake256', { outputLength: 16 });
+  var md5 = crypto.createHash('md5');
   const hex = md5.update(url).digest('hex')
   return hex.slice(8, 24)
 }
